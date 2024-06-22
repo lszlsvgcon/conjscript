50ca2d0362fee72a6be3e8cce604760719198621
lszlsvgcon <lsfmaillsvl@gmail.com>
2024-06-22 15:48:03 -0300

    conexão com javascript e três impressões no terminal

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+function imprimirMensagem() {
+  console.log("Conexão feita com sucesso!");
+}
+
+// Chama a função imprimirMensagem três vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
